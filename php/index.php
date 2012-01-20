a9ff561e79bd4e6dcd2246eae00887e39da79499
Kees <kees20055@live.nl>
2012-01-20 13:28:43 +0100

    php test files

---
+++ php/index.php
@@ -0,0 +1,118 @@
+﻿<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
+	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
+
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		<title>
+			Patriciaat Forum
+		</title>
+	</head>
+
+	<body>
+		<div class="container">
+
+
+
+
+			<div class="header">
+			<A HREF="home.html">Het Patriciaat Forum</A>
+			</div>
+
+			<div class="menu">
+
+				<div class="menuknop">
+				<A HREF="home.html">Home</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="login.html">Login</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="register.html">Register</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="personal.html">Personal</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="admin.html">Admin</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="faq.html">FAQ</A>
+				</div>
+
+			</div>
+			
+			
+			<div class="slidemenu">
+
+				 <div class="forum">
+				 	Forum
+				 </div>
+				 <div class="categorie">
+				 	<br />
+				 	 &#8627; Categorie
+				 </div>
+				 <div class="thread">
+				 	 &#8627; Thread
+				 </div>
+				 <div class="reactie">
+				 	 &#8627; Reactie
+				 </div>
+			</div>
+			
+			<div class="center">
+			
+			
+			<div class="login">
+						<?php
+						$dbhost = 'websec.science.uva.nl';
+$dbuser = 'webdb1236';
+$dbpass = 'wexet4eb';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die                      ('Error connecting to mysql');
+
+$dbname = 'users';
+mysql_select_db($dbname);?>
+						Username: <input type="text" name="User name" />
+						<br />
+						Password: <input type="password" name="pwd" />
+						<br />
+						<input type="checkbox" name="remember" value="Password" /> Remember me
+						<button type="button">Submit</button>
+						<br />
+						<a href="registreerpagina">Registreer</a>
+				</div>
+			
+
+			<?php
+// an example of closedb.php
+// it does nothing but closing
+// a mysql database connection
+
+mysql_close($conn);
+?>
+
+			
+			</div>
+			
+
+			<div class="footer">
+				&#169; 2012 Patriciaat 
+			</div>
+
+		</div>
+
+	</body>
+
+
+
+
+
+</html>
