24b2f8e460e5917a578fbd3aeb4db216c0e82009
Kees <kees20055@live.nl>
2012-01-23 13:42:24 +0100

    login pagina toegevoegd, nog niet functioneel

---
+++ php/login.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
+	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
+
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		<title>
+			Patriciaat Forum
+		</title>
+	</head>
+
+	<body>
+		<div class="container">
+
+
+
+
+			<div class="header">
+			<A HREF="home.html">Het Patriciaat Forum</A>
+			</div>
+
+			<div class="menu">
+
+				<div class="menuknop">
+				<A HREF="home.html">Home</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="login.html">Login</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="register.html">Register</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="personal.html">Personal</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="admin.html">Admin</A>
+				</div>
+
+				<div class="menuknop">
+				<A HREF="faq.html">FAQ</A>
+				</div>
+
+			</div>
+			
+			
+			<div class="slidemenu">
+
+				 <div class="forum">
+				 	Forum
+				 </div>
+				 <div class="categorie">
+				 	<br />
+				 	 &#8627; Categorie
+				 </div>
+				 <div class="thread">
+				 	 &#8627; Thread
+				 </div>
+				 <div class="reactie">
+				 	 &#8627; Reactie
+				 </div>
+			</div>
+			
+			<div class="center">
+			
+			
+			<div class="login">
+					<form action="checklogin.php" method="POST">
+						Username: <input type="text" name="username" />
+						<br />
+						Password: <input type="password" name="password />
+						<br />
+						<input type="checkbox" name="remember" value="Password" /> Remember me
+						<button type="button">Submit</button>
+						<br />
+						<a href="registreerpagina">Registreer</a>
+					</form>
+				</div>
+			
+
+			
+			
+			</div>
+			
+
+			<div class="footer">
+				&#169; 2012 Patriciaat 
+			</div>
+
+		</div>
+
+	</body>
+
+
+
+
+
+</html>
