e2c98b99d17d6671fb4bf2af965160d950fce585
Kees <kees20055@live.nl>
2012-01-27 13:15:01 +0100

    slidemenu fix, is nu af

--- php/slidemenu.php
+++ php/slidemenu.php
@@ -1,25 +1,47 @@
-<?php session_start(test); ?>
 <div class="forum">
-	<a href="..">Forum</a>
+	<a href="index.php">Forum</a>
 </div>
 <div class="categorie">
 	<br />
 	<?php 
-	include 'dblogin.php';
-	if (!empty($_GET["cat"]))
-	{
-		$cat = strip_tags($_GET["cat"]);
-		$cats = mysql_query("SELECT name FROM catagories WHERE id='$cat'");
-		$cats = mysql_fetch_array($cats);
-		echo "&#8627; <a href='category.php?cat=".$cat."'>".$cats['name']."</a>";
-	}
+		// Functie neemt naam van het bestand minus extensie.
+		function curPageName() 
+		{
+			 return substr($_SERVER["SCRIPT_NAME"],
+			 strrpos($_SERVER["SCRIPT_NAME"],"/")+1,
+			 strrpos($_SERVER["SCRIPT_NAME"],".") -1 );
+		}
+		
+		// Functie pakt bestandsnaam van huidige pagina.
+		function curPageFile() 
+		{
+			 return substr($_SERVER["SCRIPT_NAME"],
+			 strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+		}
+		
+		// Database connectie.
+		include 'dblogin.php';
+		
+		if (!empty($_GET["cat"]))
+		{
+			$cat = strip_tags($_GET["cat"]);
+			$cats = mysql_query("SELECT name FROM catagories WHERE id='$cat'");
+			$cats = mysql_fetch_array($cats);
+			echo "&#8627; <a href='category.php?cat=".$cat."'>".$cats['name']."</a>";
+		}
+		else 
+			echo "&#8627; <a href='".curPageFile()."'>".curPageName()."</a>";
 	?>
 </div>
 <div class="thread">
 	<?php 
 	if (!empty($_GET["topicid"]))
-	{
+	{ 
 		$topic = strip_tags($_GET["topicid"]);
+		
+		//if (!empty($_GET["id"])
+		//	$topic = strip_tags($_GET["id"]);
+		
 		$topics = mysql_query("SELECT * FROM topics WHERE post_id='$topic'");
 		$topics = mysql_fetch_array($topics);
 		
