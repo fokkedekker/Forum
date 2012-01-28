313cf7368fe1d19999767da0867e0d79c07ebb10
Kees <kees20055@live.nl>
2012-01-28 12:55:22 +0100

    draadje.php footer fix

--- php/draadje.php
+++ php/draadje.php
@@ -3,40 +3,27 @@
 	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
 
 <html>
-
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<title>
 			Patriciaat Forum
 		</title>
 	</head>
-
 	<body>
 		<div class="container">
-
-
-
-
 			<div class="header">
-			<A HREF="home.html">Het Patriciaat Forum</A>
+				<a href="index.php">Het Patriciaat Forum</a>
 			</div>
-
 			<div class="menu">
-
 				<?php include 'menu.php' ?>
-
 			</div>
-			
-			
 			<div class="slidemenu">
 				<?php include 'slidemenu.php' ?>
 			</div>
-			
 			<div class="center">
 				<?php 
 					include 'dblogin.php';
 					
-					//TODO juiste draad weergeven
 					$getPostID = $_GET['topicid'];
 					$getCat = $_GET['cat'];
 					$result = mysql_query("SELECT * FROM topics WHERE approved = '0' and post_id = '$getPostID' ORDER BY starttime ASC") or die(mysql_error());  
@@ -58,20 +45,10 @@
 					}
 					mysql_close($dbhandle);
 				?>
-				</div>	
-			</div>
-			
-
+			</div>	
 			<div class="footer">
 				&#169; 2012 Patriciaat 
 			</div>
-
 		</div>
-
 	</body>
-
-
-
-
-
 </html>
