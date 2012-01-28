79d93fef20070599ad80c229a462907b1b4fe1e1
Kees <kees20055@live.nl>
2012-01-28 13:11:41 +0100

    Je moet nu ingelogd zijn om te kunnen posten.

--- php/maketopic.php
+++ php/maketopic.php
@@ -1,77 +1,64 @@
 <?php session_start('test'); ?>
+
 <!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
 	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
 
 <html>
-
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<title>
 			Patriciaat Forum
 		</title>
 	</head>
-
 	<body>
 		<div class="container">
-
-
-
-
 			<div class="header">
 			<A HREF="home.html">Het Patriciaat Forum</A>
 			</div>
-
 			<div class="menu">
-
 				<?php include 'menu.php' ?>
-
 			</div>
-			
-			
 			<div class="slidemenu">
 				<?php include 'slidemenu.php' ?>
 			</div>
-			
 			<div class="center">
-			
-			
-			<div class="newtopic">
-				Topicname:<br \>
-				Message:<br \>
 				<?php 
-					//TODO redirect als niet ingelogd
-					$topic = strip_tags($_GET["topic"]);
-					
-					if ($topic === 'f')
-						$id = strip_tags($_GET["id"]);
-					else
-						$id = 0;
+					if (!empty($_SESSION['admin']))
+					{
+						echo "<div class='newtopic'>
+							Topicname:<br \>
+							Message:<br \>";
 						
-					$cat = strip_tags($_GET["cat"]);
+						$topic = strip_tags($_GET["topic"]);
+						
+						if ($topic === 'f')
+							$id = strip_tags($_GET["id"]);
+						else
+							$id = 0;
+							
+						$cat = strip_tags($_GET["cat"]);
+						
+						$url = "posttopic.php?id=".$id."&topic=".$topic."&cat=".$cat;
 					
-					$url = "posttopic.php?id=".$id."&topic=".$topic."&cat=".$cat;
+						echo "<form action= ".$url." method='POST'>
+							<input type='submit' value='Submit'/>
+							</div>
+							<div>
+									<input type='text' size='40' maxlength='64' name='topicname' /><br />
+									<input type='text' size='40' maxlength='4096' style='height:200px;' name='content' />
+								</form>
+							</div>";
+					}
+					else
+					{
+						echo "You need to be logged in in order to post.<br />";
+						echo "Click <a href='login.php'>here</a> to login.";
+					}
 				?>
-				<form action=<?php echo $url; ?> method="POST">
-					<input type="submit" value="Submit"/>
 			</div>
-			<div>
-					<input type="text" size="40" maxlength="64" name="topicname" /><br />
-					<input type="text" size="40" maxlength="4096" style="height:200px;" name="content" />
-				</form>
-			</div>
-			</div>
-			
-
 			<div class="footer">
 				&#169; 2012 Patriciaat 
 			</div>
-
 		</div>
-
 	</body>
-
-
-
-
-
 </html>
