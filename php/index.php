f523ed7a76d1078aeb380e885c3620eb0e9c0e1c
Kees <kees20055@live.nl>
2012-01-28 12:51:29 +0100

    link in header fix bij index.php

--- php/index.php
+++ php/index.php
@@ -1,10 +1,9 @@
-<?php session_start(test); ?>
-<?xml version="1.0"?>
+<?php session_start('test'); ?>
+
 <!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
 	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
 
 <html>
-
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<title>
@@ -15,15 +14,12 @@
 		<div class="container">
 
 			<div class="header">
-				<A HREF="home.html">Het Patriciaat Forum</A>
+				<A HREF="index.php">Het Patriciaat Forum</A>
 			</div>
 
 			<div class="menu">
-			
 				<?php include 'menu.php' ?>
-
 			</div>
-			
 			
 			<div class="slidemenu">
 				<?php include 'slidemenu.php' ?>
