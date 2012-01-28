d78b64e96e0c96fbba891bed550831c6a0d20ab9
Kees <kees20055@live.nl>
2012-01-28 11:57:30 +0100

    menu.php hoeft geen session te laden omdat hij anders een fout geeft dat er al een actieve sessie is van test

--- php/menu.php
+++ php/menu.php
@@ -1,24 +1,23 @@
-<?php session_start('test'); ?>
-	<div class="menuknop">
-	<A HREF="index.php">Home</A>
-	</div>
+<div class="menuknop">
+<A HREF="index.php">Home</A>
+</div>
 
-	<div class="menuknop">
-	<A HREF="login.php">Login</A>
-	</div>
+<div class="menuknop">
+<A HREF="login.php">Login</A>
+</div>
 
-	<div class="menuknop">
-	<A HREF="register.php">Register</A>
-	</div>
+<div class="menuknop">
+<A HREF="register.php">Register</A>
+</div>
 
-	<div class="menuknop">
-	<A HREF="personal.php">Personal</A>
-	</div>
+<div class="menuknop">
+<A HREF="personal.php">Personal</A>
+</div>
 
-	<div class="menuknop">
-	<A HREF="adminpage.php">Admin</A>
-	</div>
+<div class="menuknop">
+<A HREF="adminpage.php">Admin</A>
+</div>
 
-	<div class="menuknop">
-	<A HREF="faq.php">FAQ</A>
-	</div>
+<div class="menuknop">
+<A HREF="faq.php">FAQ</A>
+</div>
