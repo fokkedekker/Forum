f5fe553494904bef7c4835bb15b1ab525b4b90da
Kees <kees20055@live.nl>
2012-01-30 17:21:27 +0100

    Voor Hamid.

--- php/changing.php
+++ php/changing.php
@@ -1,4 +1,4 @@
-
+<?php session_start('test'); ?>
 <!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
 	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
 
@@ -28,91 +28,22 @@
 			<div class="center">
 			
 			<?php
-			
-			
-			
-			session_start('test');
-			
-			$user = $_SESSION['username'];
-			
-			if ($user)
+			if (!empty($_SESSION['username']))
 			{
-			
-			if ($_POST['sumbit'])
-			{
-			$oldpassword = $_POST['oldpassword'];
-			$newpassword = $_POST['newpassword'];
-			$repeatnewpassword = $_POST['repeatnewpassword'];
-
-			//check password db
-			include 'dblogin.php';
-			
-			$queryget = mysql_query("SELECT password FROM users WHERE username='$user") or die (" Password NOT changed succesfully!!" ); 
-			$row = mysql_fetch_assoc($queryget);
-			
-			$oldpassworddb = $row['password'];
-			
-			
-			//check password
-			
-			if ($oldpassword==$oldpassworddb)
-			{
-			if ($newpassword==$repeatnewpassword)
-			{
-			//success & change
-			
-			echo "succeded!";
-			
-			$querychange = mysql_query("UPDATE users SET password='$newpassword' WHERE username='$user'");
-			
-			session_destroy();
-			
-			die ("Your password has been changed!. < a href='index.php'>Return</a> to the main page.");
-			
-			}
-			else
-				die("New passwords don't match!"); 
-			
-			}
-			else 
-				die ("Old password don't match!");
+				echo "
+				<form action='changepw.php' method='POST'>
+				Old password: <input type= 'password' name='oldpassword'><p>
+				New password: <input type='password' name='newpassword'><br>
+				Repeat new password: <input type='password' name='reapeatnewpassword'><p>
+				<input type='submit' name='submit' value='change password'>
+				</form>";
 				
-			
-			
-			}
-			
-			else
-			{
-			
-			echo"
-			
-			<form action='changing.php' method='POST'>
-			Old password: <input type= 'password' name='oldpassword'><p>
-			New password: <input type='password' name='newpassword'><br>
-			Repeat new password: <input type='password' name='reapeatnewpassword'><p>
-			<input type='submit' name='submit' value='change password'>
-			
-			</form>
-			";
-			}
-			
 			}
 			else
-			
+			{	
 				die("You must be logged in to change your password!");
+			}
 				
-				
-	
-			
-				
-				
-				
-				
-				
-				
-				
-				
-			mysql_close($dbhandle);
 				?>
 			</div>
 			
