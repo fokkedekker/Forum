ec938b9ab6852d269209e1bd79197459cadfe897
michiel mosmans <michielmosmans@hotmail.com>
2012-01-30 11:51:09 +0100

    repush?

--- php/adminpage.php
+++ php/adminpage.php
@@ -5,6 +5,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<<<<<<< HEAD
 			<script type="text/javascript">
 				function showTopic(str)
 				{
@@ -39,6 +40,37 @@
 					xmlhttp.send();
 				}
 			</script>
+=======
+<script type="text/javascript">
+function showTopic(str)
+{
+if (str=="")
+  {
+  document.getElementById("topicTabel").innerHTML="";
+  return;
+  } 
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("topicTabel").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","adminphp.php?q="+str,true);
+xmlhttp.send();
+}
+</script>
+
+		
+>>>>>>> 9cc4fb1... adminpage met ajax
 		<title>
 			Patriciaat Forum
 		</title>
@@ -52,6 +84,7 @@
 			</div>
 
 			<div class="menu">
+<<<<<<< HEAD
 			<?php include 'menu.php'; ?>
 			</div>
 			
@@ -68,11 +101,40 @@
 				</form>
 				<br />
 				<div id="topicTabel""></div>
+=======
+			<?php include 'menu.php' ?>
 			</div>
+			
+			<div class="slidemenu">
+			<a href="adminpagecat.php">Add Catagory</a>
+			</div>
+
+			<div class="center">
+			
+			<form>
+			<select name="users" onchange="showTopic(this.value)">
+			<option value="">Select Topic Type:</option>
+			<option value="0">Pending Topics</option>
+			<option value="1">Approved Topics</option>
+			</select>
+			</form>
+			<br />
+			<div id="topicTabel">Select topics will be displayed here</div>
+
+>>>>>>> 9cc4fb1... adminpage met ajax
+			</div>
+
 
 			<div class="footer">
 				&#169; 2012 Patriciaat 
 			</div>
+
 		</div>
+
 	</body>
+
+
+
+
+
 </html>
---
+++ php/adminphp.php
@@ -0,0 +1,37 @@
+<?php
+$q=$_GET["q"];
+
+include"dblogin.php";
+
+$sql="SELECT * FROM topics where id = '".$q."'";
+
+$result = mysql_query($sql);
+
+while($row = mysql_fetch_array($result))
+  {
+		echo "<div class='topic_id'>";
+		
+		echo $row['id'];
+		echo "</div>";
+
+		echo "<div class='topic_title'>";
+		echo "<a href='draadje.php?topicid=".$row['post_id']."&cat=".$row['catagorie_id']."'>".$row['posttitle']."</a>";
+		echo "</div>";
+
+		echo "<div class='topic_starttime'>";
+		echo $row['starttime'];
+		echo "</div>";
+
+		echo "<div class='topic_action'>";
+		echo "<form style='float: left;' action='approve.php?id=".$row['id']."' method='POST'>";
+		echo "<input type='submit' value='Y'/>";
+		echo "</form>";
+		echo "<form action='delete.php?id=".$row['id']."' method='POST'>";
+		echo "<input type='submit' value='X'/>";
+		echo "</form>";
+
+		echo "</div>";
+  }
+
+?>
+
