9a03fa23698afdcb19fc903febd9ca8030c6a9fe
Kees <kees20055@live.nl>
2012-01-30 16:16:14 +0100

    ADmin updates en posttopic

--- php/adminpage.php
+++ php/adminpage.php
@@ -5,35 +5,40 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
-<script type="text/javascript">
-function showTopic(str)
-{
-if (str=="")
-  {
-  document.getElementById("topicTabel").innerHTML="";
-  return;
-  } 
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("topicTabel").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("GET","adminphp.php?q="+str,true);
-xmlhttp.send();
-}
-</script>
-
-		
+			<script type="text/javascript">
+				function showTopic(str)
+				{
+					  if (str=="0")
+					  {
+						document.getElementById("topicTabel").innerHTML="Pending";
+						str = "0";
+					  }
+					  else
+					  {
+						document.getElementById("topicTabel").innerHTML="Approved";
+						str = "1";
+					  }
+					  
+					  if (window.XMLHttpRequest)
+					  {// code for IE7+, Firefox, Chrome, Opera, Safari
+						xmlhttp=new XMLHttpRequest();
+					  }
+					  else
+					  {// code for IE6, IE5
+						xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+					  }
+					  
+					  xmlhttp.onreadystatechange=function()
+					  {
+					  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+						{
+						document.getElementById("topicTabel").innerHTML=xmlhttp.responseText;
+						}
+					  }
+					xmlhttp.open("GET","adminshow.php?q="+str,true);
+					xmlhttp.send();
+				}
+			</script>
 		<title>
 			Patriciaat Forum
 		</title>
@@ -47,38 +52,27 @@ xmlhttp.send();
 			</div>
 
 			<div class="menu">
-			<?php include 'menu.php' ?>
+			<?php include 'menu.php'; ?>
 			</div>
 			
 			<div class="slidemenu">
-			<a href="adminpagecat.php">Add Catagory</a>
+				<?php include 'adminslide.php'; ?>
 			</div>
 
 			<div class="center">
-			
-			<form>
-			<select name="users" onchange="showTopic(this.value)">
-			<option value="">Select Topic Type:</option>
-			<option value="0">Pending Topics</option>
-			<option value="1">Approved Topics</option>
-			</select>
-			</form>
-			<br />
-			<div id="topicTabel">Select topics will be displayed here</div>
-
+				<form>
+					<select name="users" onchange="showTopic(this.value)">
+						<option value="0">Pending Topics</option>
+						<option value="1">Approved Topics</option>
+					</select>
+				</form>
+				<br />
+				<div id="topicTabel""></div>
 			</div>
-
 
 			<div class="footer">
 				&#169; 2012 Patriciaat 
 			</div>
-
 		</div>
-
 	</body>
-
-
-
-
-
 </html>
--- php/approve.php
+++ php/approve.php
@@ -29,16 +29,16 @@
 			</div>
 
 			<div class="center">
-<?php
-include 'dblogin.php';
+				<?php
+					include 'dblogin.php';
 
-$approve_topic = strip_tags($_GET['id']);
-mysql_query("UPDATE topics SET approved='1' WHERE id='$approve_topic'") or die (mysql_error());
+					$approve_topic = strip_tags($_GET['id']);
+					mysql_query("UPDATE topics SET approved='1' WHERE id='$approve_topic'") or die (mysql_error());
 
-echo "Topic approved";
+					echo "Topic approved";
 
-?>			
-		</div>
+				?>			
+			</div>
 
 
 			<div class="footer">
