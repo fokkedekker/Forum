b6f64b188b54a137a26c9bc831cf264c5a68a2ef
Fokke Dekker <fokke.dekker@me.com>
2012-02-01 16:17:56 +0100

    register user name to long

--- php/insert.php
+++ php/insert.php
@@ -39,7 +39,7 @@
 
 									$userpass = md5($random);
 
-									if($str1 == $str2 && !(strlen($username) > 12))
+									if($str1 == $str2 && (strlen($username) < 13 ))
 									{
 										$sql="INSERT INTO users (first_name, last_name, email, favo_browser, personal_info, date_of_birth, username, sex, password)
 										VALUES
@@ -72,6 +72,11 @@
 										echo "emails zijn niet gelijk";
 									}
 									// Database connectie afsluiten.
+									
+									else if(strlen($username) > 12 )
+									{
+										echo "username is to long";
+									}
 									
 				
 									
