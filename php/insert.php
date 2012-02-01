00d75a038b4024ad8fe99b14f6796ff17a640b03
Fokke Dekker <fokke.dekker@me.com>
2012-02-01 09:30:58 +0100

    verbeterde mail

--- php/insert.php
+++ php/insert.php
@@ -1,43 +1,43 @@
 <?php session_start('test'); ?>
 <?php include "dblogin.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
-	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
+        "http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
 
 <html>
 
-	<head>
-		<link rel="stylesheet" type="text/css" href="stylesheet.css">
-		<title>
-			Patriciaat Forum
-		</title>
-	</head>
+        <head>
+                <link rel="stylesheet" type="text/css" href="stylesheet.css">
+                <title>
+                        Patriciaat Forum
+                </title>
+        </head>
 
-	<body>
-		<div class="container">
-			<div class="header">
-				<a href="index.php">Het Patriciaat Forum</A>
-			</div>
+        <body>
+                <div class="container">
+                        <div class="header">
+                                <a href="index.php">Het Patriciaat Forum</A>
+                        </div>
 
-			<div class="menu">
+                        <div class="menu">
 
-				<?php include 'menu.php' ?>
+                                <?php include 'menu.php' ?>
 
-			</div>
-			
-			<div class="slidemenu">
-				<?php include 'slidemenu.php'?>
-			</div>
-			
-			<div class="center">
-				<?php
-					
+                        </div>
+
+                        <div class="slidemenu">
+                                <?php include 'slidemenu.php'?>
+                        </div>
+
+                        <div class="center">
+                                <?php
+
 
 $str1 =strip_tags($_POST["email"]);
 $str2 =strip_tags($_POST["repeatemail"]);
 $day =strip_tags($_POST["day"]);
 $month =strip_tags($_POST["month"]);
 $year =strip_tags($_POST["year"]);
-$date =($day.$month.$year); 
+$date =($day.$month.$year);
 $username = strip_tags($_POST["username"]);
 $firstname = strip_tags($_POST["firstname"]);
 $lastname = strip_tags($_POST["lastname"]);
@@ -62,9 +62,15 @@ VALUES
 
 
 $to = "$str1";
-$subject = "Your password";
-$message = "This is your password $random";
-$from = "fokke.dekker@me.com";
+$subject = "Registration for Gitmasters.com PRIVATE INFORMATION INSIDE!";
+$message = "Thank you for registering for gitmasters.
+Your username is $username.
+Your password is $random.
+
+best regards,
+
+The gitmasters team.";
+$from = "noreply@gitmasters.com";
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
 echo "Mail Sent.";
@@ -74,27 +80,29 @@ if (!mysql_query($sql,$dbhandle))
   {
   die('Error: ' . mysql_error());
   }
-echo "1 record added";
+echo "Thank you for registering for gitmasters.com. <br/>
+A message containing your log in credentials have been send to your E-mail <br/>";
 
 }
 
 
 else if($str1 != $str2)
 {
-	echo "emails zijn niet gelijk";
-	
+        echo "emails zijn niet gelijk";
+
 }
 
 
-					
-					// Database connectie afsluiten.
-					mysql_close($dbhandle);
-				?>
-			</div>
-			
-			<div class="footer">
-				&#169; 2012 Patriciaat 
-			</div>
-		</div>
-	</body>
+
+                                        // Database connectie afsluiten.
+                                        mysql_close($dbhandle);
+                                ?>
+                        </div>
+
+                        <div class="footer">
+                                &#169; 2012 Patriciaat
+                        </div>
+                </div>
+        </body>
 </html>
+                              
