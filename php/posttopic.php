11ce417ed9f4267f3d5350b0f7f5e0cd52e8eb35
Kees <kees20055@live.nl>
2012-02-02 14:55:11 +0100

    login fix en leeg ding met foutmelding

--- php/posttopic.php
+++ php/posttopic.php
@@ -31,97 +31,94 @@
 					$postTitle = mysql_real_escape_string(strip_tags($_POST["topicname"]));
 					$postContent = mysql_real_escape_string(strip_tags($_POST["content"]));
 					
-					// Haal gebruikersnaam uit sessie.
-					$username = mysql_real_escape_string($_SESSION['username']);
-					
-					// Haal user ID uit sessie.
-					$userID = mysql_real_escape_string($_SESSION['userID']);
-					
-					// Verkrijgt catagorie_id.
-					$catagory = mysql_real_escape_string($_GET['cat']);
-					
-					// Laadt catagorie approval.
-					$approval = mysql_query("SELECT approval FROM catagories where id = '$catagory'") or die ("Oops something went wrong you can try again in a few minutes.");
-					$approval = mysql_fetch_array($approval);
-					
-					// Prompt gebruiker.
-					echo "Thank you for your submission. <br />";
-										
-					// Checkt of er een nieuw draad aangemaakt wordt of het een reply is.
-					$checkThread = mysql_real_escape_string(strip_tags($_GET["topic"]));
-					
-					if ($checkThread === "y")
+					if($postTitle != "" && $postContent !="" && array_key_exists('admin',$_SESSION))
 					{
-						// Berekent nieuwe post id op basis van max in tabel.
-						$getPost_id = mysql_query("SELECT MAX(post_id) as post_id FROM topics") or die ("Oops something went wrong you can try again in a few minutes.");
-						$row = mysql_fetch_array($getPost_id);
-						$newPost_id = $row['post_id'] + 1;
-						$start = 1;
-					} 
+						// Haal gebruikersnaam uit sessie.
+						$username = mysql_real_escape_string($_SESSION['username']);
+						
+						// Haal user ID uit sessie.
+						$userID = mysql_real_escape_string($_SESSION['userID']);
+						
+						// Verkrijgt catagorie_id.
+						$catagory = mysql_real_escape_string($_GET['cat']);
+						
+						// Laadt catagorie approval.
+						$approval = mysql_query("SELECT approval FROM catagories where id = '$catagory'") or die (mysql_error());
+						$approval = mysql_fetch_array($approval);
+						
+						// Prompt gebruiker.
+						echo "Thank you for your submission. <br />";
+											
+						// Checkt of er een nieuw draad aangemaakt wordt of het een reply is.
+						$checkThread = mysql_real_escape_string(strip_tags($_GET["topic"]));
+						
+						if ($checkThread === "y")
+						{
+							// Berekent nieuwe post id op basis van max in tabel.
+							$getPost_id = mysql_query("SELECT MAX(post_id) as post_id FROM topics") or die (mysql_error());
+							$row = mysql_fetch_array($getPost_id);
+							$newPost_id = $row['post_id'] + 1;
+							$start = 1;
+						} 
+						else
+						{
+							// Set post_id naar die van uit de GET van de url.
+							$newPost_id = strip_tags($_GET["id"]);
+							$start = 0;
+						}
+						
+						// Prompt approval.
+						if ($approval['approval'] == 0)
+							echo "Your post is pending approval.<br />";
+						else
+						{
+							echo "Your post has been approved.<br />";
+							echo "Click <a href= draadje.php?topicid=".$newPost_id."&cat=".$catagory."> here </a> to visit your submission.";
+						}
+						$approval = $approval['approval'];
+						// Prompt gebruiker.
+						echo "<hr />";
+						
+						// Pompt titel van post.
+						echo "<br />Title: ".$postTitle."<br />";
+						
+						//Prompt catagorie naam.
+						$catagoryName = mysql_query("SELECT name FROM catagories WHERE id = '$catagory'") or die (mysql_error());
+						$catagoryName = mysql_fetch_array($catagoryName);
+						$catagoryName = $catagoryName['name'];
+						echo "Catagory: ".$catagoryName."<br />";
+						
+						// Prompt username.
+						echo "Username: <a href=personal.php?id=".$userID.">".$username."</a><br />";
+						
+						// Prompt postID.
+						echo "PostID: ".$newPost_id."<br />";
+						
+						// Prompt content
+						echo "<br />Content: ".$postContent."<br />";
+						
+						// Mysql query.
+						mysql_query("INSERT INTO `webdb1236`.`topics`
+						(approved, 
+						posttitle, 
+						postcontent, 
+						post_id, 
+						catagorie_id, 
+						user_id, 
+						starttime,
+						start)
+						VALUES ('$approval', 
+						'$postTitle', 
+						'$postContent', 
+						'$newPost_id', 
+						'$catagory', 
+						'$userID', 
+						CURRENT_TIMESTAMP,
+						'$start')") or die (mysql_error());
+					}
 					else
 					{
-						// Set post_id naar die van uit de GET van de url.
-						$newPost_id = strip_tags($_GET["id"]);
-						$start = 0;
-					}
-					
-					// Prompt approval.
-					if ($approval['approval'] == 0)
-						echo "Your post is pending approval.<br />";
-					else
-					{
-						echo "Your post has been approved.<br />";
-						echo "Click <a href= draadje.php?topicid=".$newPost_id."&cat=".$catagory."> here </a> to visit your submission.";
-					}
-					$approval = $approval['approval'];
-					// Prompt gebruiker.
-					echo "<hr />";
-					
-					// Pompt titel van post.
-					echo "<br />Title: ".$postTitle."<br />";
-					
-					//Prompt catagorie naam.
-					$catagoryName = mysql_query("SELECT name FROM catagories WHERE id = '$catagory'") or die ("Oops something went wrong you can try again in a few minutes.");
-					$catagoryName = mysql_fetch_array($catagoryName);
-					$catagoryName = $catagoryName['name'];
-					echo "Catagory: ".$catagoryName."<br />";
-					
-					// Prompt username.
-					echo "Username: <a href=personal.php?id=".$userID.">".$username."</a><br />";
-					
-					// Prompt postID.
-					echo "PostID: ".$newPost_id."<br />";
-					
-					// Prompt content
-					echo "<br />Content: ".$postContent."<br />";
-					
-					// Mysql query.
-					if($postTitle != "" && $postContent !="")
-					{
-					
-					mysql_query("INSERT INTO `webdb1236`.`topics`
-					(approved, 
-					posttitle, 
-					postcontent, 
-					post_id, 
-					catagorie_id, 
-					user_id, 
-					starttime,
-					start)
-					VALUES ('$approval', 
-					'$postTitle', 
-					'$postContent', 
-					'$newPost_id', 
-					'$catagory', 
-					'$userID', 
-					CURRENT_TIMESTAMP,
-					'$start')") or die ("Oops something went wrong you can try again in a few minutes.");
-					
-					}
-					
-					else if($postTitle == "" || $postContent == "")
-					{
-						echo "not al fields ar filled in correct";
+						echo "An error has occured, cannot post, please try again!";
 					}
 					
 					// Connectie met database afsluiten.
