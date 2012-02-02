e4e8be3f010ac169f0c7fa94a1dfa23629efc95e
Kees <kees20055@live.nl>
2012-02-02 11:11:12 +0100

    personal.php

--- php/personal.php
+++ php/personal.php
@@ -1,10 +1,10 @@
 <?php 
 	session_start('test'); 
-	/*if($_SERVER['HTTPS']!=”on”)
+	if($_SERVER['HTTPS']!="on")
 	{
 		$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 		header("Location:$redirect");
-	}*/
+	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.1//EN"
 	"http://www.w3.org/ter/xhtml11/DTD/xhtml11.dtd">
